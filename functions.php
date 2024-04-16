e65743557ddeaa12b104134f05652b773003bb46
PolinaAndreyanova <andreyanovalina@yandex.ru>
2024-04-16 20:56:20 +0000

    добавить валидацию полей и поздравление

--- functions.php
+++ functions.php
@@ -12,6 +12,95 @@ function PostDataHandler(): array
     ];
 }
 
+function NameValidation(string $name): bool
+{
+    $regex = '/^([а-яА-ЯёЁ]+)$/s';
+    return preg_match($regex, $name);
+}
+
+function EmailValidation(string $email): bool
+{
+    $regex = '/(^([a-zA-Z0-9][a-zA-Z0-9._-]{0,}[a-zA-Z0-9]{0,})@(?:[a-zA-Z0-9][a-zA-Z0-9_-]{0,}[a-zA-Z0-9]{0,}\.)+[a-zA-Z0-9][a-zA-Z0-9_-]{0,}[a-zA-Z0-9]{0,}$)/s';
+    return preg_match($regex, $email);
+}
+
+function PhoneValidation(string $phone): bool
+{
+    $regex = '/^(\+[0-9]{11})$/s';
+    return preg_match($regex, $phone);
+}
+
+function HandleValidation(array $arUserData): array
+{
+    $arValidationErrors = [];
+
+    if (!PhoneValidation($arUserData["phone"])) {
+        $arValidationErrors["phone"] = "Поле не соответствует формату номера телефона";
+    }
+
+    if (!NameValidation($arUserData["surname"])) {
+        $arValidationErrors["surname"] = "Поле должно быть заполнено кириллицей";
+    }
+
+    if (!NameValidation($arUserData["name"])) {
+        $arValidationErrors["name"] = "Поле должно быть заполнено кириллицей";
+    }
+
+    if (!NameValidation($arUserData["secondName"])) {
+        $arValidationErrors["secondName"] = "Поле должно быть заполнено кириллицей";
+    }
+
+    if (!EmailValidation($arUserData["email"])) {
+        $arValidationErrors["email"] = "Поле не соответствует формату email";
+    }
+
+    if (!$arUserData["message"]) {
+        $arValidationErrors["message"] = "Сообщение не может быть пустым";
+    }
+
+    if (strtotime($arUserData["dateOfBirth"]) > strtotime("now")) {
+        $arUserData["dateOfBirth"] = "День рождения не может быть в будущем!";
+    }
+
+    return $arValidationErrors;
+}
+
+function HandleError(string $key, array $arValidationErrors): string
+{
+    if (array_key_exists($key, $arValidationErrors)) { 
+        return $arValidationErrors[$key];
+    }
+    return "";
+}
+
+function IsBirthdayToday(string $date): int
+{
+    $today = new DateTime();
+    $targetDate = new DateTime($date);
+
+    $targetDate->setDate($today->format("Y"), $targetDate->format("m"), $targetDate->format("d"));
+
+    $diff = $today->diff($targetDate);
+    
+    if ($diff->invert) {
+        $targetDate->modify("+1 year");
+        $diff = $today->diff($targetDate);
+    }
+    
+    return $diff->days + 1;
+}
+
+function HandleIsBirthdayToday(array $arUserData): string
+{
+    $isBirthdayToday = IsBirthdayToday($arUserData["dateOfBirth"]);
+
+    if ($isBirthdayToday === 365) {
+        return " С Днем Рождения, " . $arUserData["name"] . "!";
+    } else {
+        return " До Вашего Дня Рождения " . $isBirthdayToday . " дней";
+    }
+}
+
 function SendEmail(array $arUserData): bool
 {
     $username = "Rti2paBWiMuH";
@@ -25,7 +114,7 @@ function SendEmail(array $arUserData): bool
     $smtp = new SendMailSmtpClass($username, $password, $host, $from, $port, $charset);
 
     $site = $_SERVER['SERVER_NAME'];
-    $subject = "На сайте $site зарегестрирован новый пользователь";
+    $subject = "На сайте $site новое сообщение";
 
     $message = "Сообщение пользователя " . $arUserData["surname"] . " " . $arUserData["name"] . " " . $arUserData["secondName"] . ":\n\n";
     $message .= $arUserData["message"];
--- index.php
+++ index.php
@@ -18,15 +18,32 @@ $arUserData = [
     "secondName" => ""
 ];
 
+$arValidationErrors = [];
+
 $feedback = "";
 
 if ($_POST) {
     $arUserData = PostDataHandler();
+    $arValidationErrors = HandleValidation($arUserData);
 
-    if (!in_array($arUserData, $_SESSION["users"]) && SendEmail($arUserData)) {
-        $feedback = "Сообщение успешно отправлено!";
-        $_SESSION["users"][] = $arUserData;
-    };
+    if (!in_array($arUserData, $_SESSION["users"])) {
+        if (!$arValidationErrors) {
+            if (SendEmail($arUserData)) {
+                $feedback = "Сообщение успешно отправлено!<br>";
+                $feedback .= HandleIsBirthdayToday($arUserData);
+                $_SESSION["users"][] = $arUserData;
+                $arValidationErrors = [];
+            } else {
+                $feedback = "При отправке произошла ошибка!";
+            }
+        } else {
+            $feedback = "Данные не прошли валидацию!";
+        }
+    } else {
+        $feedback = "Такие данные уже отправлены!<br>";
+        $feedback .= HandleIsBirthdayToday($arUserData);
+        $arValidationErrors = [];
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -42,20 +59,27 @@ if ($_POST) {
         <h1 class="content__header">Форма</h1>
         
         <input class="content__input" type="date" name="dateOfBirth" value="<?=$arUserData["dateOfBirth"]?>" placeholder="14.11.1985" required />
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("dateOfBirth", $arValidationErrors)?></p>
         
         <input class="content__input" type="tel" name="phone" value="<?=$arUserData["phone"]?>" placeholder="+79275643843" required/>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("phone", $arValidationErrors)?></p>
         
         <input class="content__input" type="email" name="email" value="<?=$arUserData["email"]?>" placeholder="alex85@mail.ru" required/>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("email", $arValidationErrors)?></p>
 
         <textarea class="content__textarea" name="message" placeholder="Текст сообщения" required><?=$arUserData["message"]?></textarea>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("message", $arValidationErrors)?></p>
 
         <input class="content__input" type="text" name="surname" value="<?=$arUserData["surname"]?>" placeholder="Смирнов" required/>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("surname", $arValidationErrors)?></p>
 
         <input class="content__input" type="text" name="name" value="<?=$arUserData["name"]?>" placeholder="Александр" required/>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("name", $arValidationErrors)?></p>
 
         <input class="content__input" type="text" name="secondName" value="<?=$arUserData["secondName"]?>" placeholder="Иванович" required/>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("secondName", $arValidationErrors)?></p>
 
-        <button class="content__button" type="submit">Зарегистрироваться</button>
+        <button class="content__button" type="submit">Отправить</button>
         <p class="content__feedback content__feedback_type_success"><?=$feedback?></p>
     </form>
 </body>
